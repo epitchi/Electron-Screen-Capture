3710e778e1a4f128e922d5ceefc4383b5ba8f7f6
epitchi <thienvanlea1@gmail.com>
2021-10-11 07:49:25 +0700

    preview video stream

--- src/render.js
+++ src/render.js
@@ -21,3 +21,37 @@ async function getVideoSources(){
 
     videoOptionsMenu.popup();
 }
+
+let mediaRecorder; // MediaRecorder instance to capture footage
+const recordedChunks = [];
+
+// Change the videoSource windows to record 
+async function selectSource(source){
+    
+    videoSelectBtn.innerText = source.name;
+
+    const contraints = {
+        audio: false,
+        video: {
+            mandatory: {
+                chromeMediaSource: 'desktop',
+                chromeMediaSourceId: source.id
+            }
+        }
+    };
+
+    // Create a Stream 
+    const stream = await navigator.mediaDevices.getUserMedia(contraints);
+    
+    // Preview the source in a video element
+    videoElement.srcObject = stream;    
+    videoElement.play();
+
+    // Create the Media Recorder
+    const options = { mimetype: 'video/webm; code=vp9'};
+    mediaRecorder = new MediaRecorder(stream, options);
+
+    //Register Event Handlers
+    mediaRecorder.ondataavailable = handleDataAvailable;
+    mediaRecorder.onstop = handleStop;
+}
